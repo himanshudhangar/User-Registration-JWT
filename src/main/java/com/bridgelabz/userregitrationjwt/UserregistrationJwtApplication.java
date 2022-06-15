40c2aa9b7ba33b8ae821e3179c8e5cf16c050822
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-06-15 14:16:17 +0530

    Added:Generate JWT Token for Registration and Login Process in User

---
+++ src/main/java/com/bridgelabz/userregitrationjwt/UserregistrationJwtApplication.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.userregitrationjwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistrationJwtApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To User Registration App");
+		SpringApplication.run(UserregistrationJwtApplication.class, args);
+
+	}
+
+}
